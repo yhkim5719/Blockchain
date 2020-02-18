6a41e7b81113ac66da186e9835e3d579e1c61f6f
Yong Kim <yhkim5719@gmail.com>
2020-02-17 17:08:43 -0800

    .

---
+++ p0/HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		//your code here
+		System.out.println("Hello");
+		System.out.println("Yong Kim");
+		int ans;
+		ans = -5 + 8 * 6;
+		System.out.println("-5 + 8 * 6 = " + ans);
+		ans = (55 + 9) % 9;
+		System.out.println("(55 + 9) % 9 = " + ans);
+		ans = 20 + -3 * 5 / 8;
+		System.out.println("20 + -3 * 5 / 8 = " + ans);
+		ans = (5 + 15) / 3 * 2 - 8 % 3;
+		System.out.println("something = " + ans);
+	}
+}
